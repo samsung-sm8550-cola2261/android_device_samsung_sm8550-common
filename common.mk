69a7678840f2121021aa78896e978548e60313a6
Ethan Chen <intervigil@gmail.com>
2018-12-01 13:53:00 -0800

    sm8550-common: Declare EUICC permission
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Co-authored-by: razorloves <razorloves@gmail.com>
    Change-Id: I323dce8b848b44e45ed21002950059e58719113c

--- common.mk
+++ common.mk
@@ -135,6 +135,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
+# EUICC
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.mep.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.mep.xml \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
 # Fastboot
 PRODUCT_PACKAGES += \
     fastbootd
