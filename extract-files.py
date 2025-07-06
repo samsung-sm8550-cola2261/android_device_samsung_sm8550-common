2ff4bb25dd779438aac3f20709fa0b83eceb2828
Arian <arian.kulmer@web.de>
2025-07-06 18:32:00 +0200

    sm8550-common: Enable ELF checks for audio blobs
    We converted the audio hal targets
    
    Change-Id: I6b46b5c87a2c731241a7c2e34f4271545e2b61e0

--- extract-files.py
+++ extract-files.py
@@ -9,7 +9,6 @@ from extract_utils.fixups_blob import (
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
-    lib_fixup_remove,
     lib_fixups,
     lib_fixups_user_type,
 )
@@ -39,15 +38,6 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.diaghal@1.0',
         'libsecril-client',
     ): lib_fixup_vendor_suffix,
-    (
-        'libar-acdb',
-        'libar-gsl',
-        'liblx-osal',
-        'libats',
-        'libagmclient',
-        'libpalclient',
-        'vendor.qti.hardware.AGMIPC@1.0-impl',
-    ): lib_fixup_remove,
 }
 
 blob_fixups: blob_fixups_user_type = {
