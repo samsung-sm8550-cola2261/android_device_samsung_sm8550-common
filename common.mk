d6a12d333832341f97e43f43f46ee8bbf5e80a37
cola2261 <cola2261@proton.me>
2026-02-02 18:38:41 +0500

    sm8550-common: Move sensors hal config to appropriate directory
    Change-Id: Iabfc6e3164cc568052f97acc7e133724c89883fa

--- common.mk
+++ common.mk
@@ -335,7 +335,7 @@ PRODUCT_PACKAGES += \
     sensors.dynamic_sensor_hal
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
