8f1d20b5f20377b16e95c91193ff7e31bee8761b
cola2261 <cola2261@proton.me>
2026-08-06 22:44:21 +0500

    sm8550-common: Update dependencies and replace ImsMedia
    Change Dolby hardware tree
    Add IMS dependencies
    Replace AOSP ImsMedia with a custom one
    
    Change-Id: I7a23bda67b017da273b4c3a38f5a90846eaf0bd5

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,6 @@
 # Patch source
 ./device/samsung/sm8550-common/apply-patches.sh
+
+# Replace ImsMedia
+rm -rf packages/modules/ImsMedia
+git clone https://github.com/samsung-sm8550-cola2261/ImsMedia packages/modules/ImsMedia
