e7f21d17a7cde41f1478b374cdb00104cdf798a2
cola2261 <cola2261@proton.me>
2026-08-10 19:59:07 +0500

    Revert "sm8550-common: Build OpenEUICC"
    This reverts commit 4c954df2c1eee289b0e5ce33b07a127a4fe1a5b0.
    
    Change-Id: Icd64e031c58584c2441efc184d02b9bdec98e1b0

--- common.mk
+++ common.mk
@@ -146,8 +146,7 @@ PRODUCT_PACKAGES += \
 
 # EUICC
 PRODUCT_PACKAGES += \
-    SamsungEsimSwitcher \
-    OpenEUICC
+    SamsungEsimSwitcher
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.mep.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.mep.xml \
